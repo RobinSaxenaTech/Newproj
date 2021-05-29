a1bad3d9bc758b2fe9c0b99128353280bf12538d
Robin Saxena <rssaxenarobin@gmail.com>
2021-05-29 19:58:00 +0530

    firdt commit

---
+++ src/test/Looping.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+class Looping
+{
+	 static public void main(String[] args) 
+	{
+		 final int i;
+
+		    i = 20;
+
+		    
+
+		    System.out.println(i);
+	}
+	
+	
+}
